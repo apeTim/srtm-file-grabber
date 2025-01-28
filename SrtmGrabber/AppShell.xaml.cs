17f6fd205d5c3afe61b58b6d244a07cf3dab1b01
Tim Vaulin <apetimx@gmail.com>
2025-01-29 00:45:56 +0300

    feat: initial commit

---
+++ SrtmGrabber/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SrtmGrabber;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SrtmGrabber/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SrtmGrabber;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
